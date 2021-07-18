6a4e6db716164e83458119c219a2412a545ed895
stOne <stoneskull@gmail.com>
2021-07-19 02:20:00 +1000

    is main

--- main.py
+++ main.py
@@ -12,29 +12,25 @@ def go(players):
         game.balling()
 
 
-if __name__ == '__main__':
-    while True:
-        players = input('number of players [0-2]? ')
-        if not players:
-            players = '0'
-        if players in ['0', '1', '2']:
-            break
+while True:
+    players = input('number of players [0-2]? ')
+    if not players:
+        players = '0'
+    if players in ['0', '1', '2']:
+        break
 
-    waitsecs = 7
+waitsecs = 7
 
-    print()
-    if players == '2': 
-        print('-- "W" for up and "S" for down')
-    if players != '0':
-        print('-- "I" for up and "K" for down')
-    print()
+print()
+if players == '2': 
+    print('-- "W" for up and "S" for down')
+if players != '0':
+    print('-- "I" for up and "K" for down')
+print()
 
-    for wait in range(waitsecs, 1, -1):
-        print(f'starting game in {wait} seconds')
-        sleep(1)
-
-else:
-    players = '1'
+for wait in range(waitsecs, 1, -1):
+    print(f'starting game in {wait} seconds')
+    sleep(1)
 
 try:
     go(players)
